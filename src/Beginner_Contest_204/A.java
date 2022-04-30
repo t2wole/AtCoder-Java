26892e30c46bfe697ccc4ba8de66e1c620c6992b
t2wole <t2wole@gmail.com>
2022-04-30 22:23:26 +0900

    Rock-paper-scissors

---
+++ src/Beginner_Contest_204/A.java
@@ -0,0 +1,24 @@
+package Beginner_Contest_204;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int x = Integer.parseInt(st.nextToken());
+		int y = Integer.parseInt(st.nextToken());
+		
+		if(x == y) 
+			System.out.println(x);
+		else
+			System.out.println(3 - x - y);
+	}
+}
