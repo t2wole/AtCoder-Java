0e21fc418beec7f6dc200e75b064f9bcd3a5499c
t2wole <t2wole@gmail.com>
2022-05-02 22:57:57 +0900

    Repression

---
+++ src/Beginner_Contest_207/A.java
@@ -0,0 +1,23 @@
+package Beginner_Contest_207;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		int c = Integer.parseInt(st.nextToken());
+		
+		int ans = (a + b + c) - Math.min(a, Math.min(b, c));
+		System.out.println(ans);
+	}
+}
