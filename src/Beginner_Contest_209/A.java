6a60e324c70f88c38e2b136fa628ce4d5aa09e83
authorsyok <authorsyok@gmail.com>
2022-05-03 16:26:42 +0900

    Counting

---
+++ src/Beginner_Contest_209/A.java
@@ -0,0 +1,24 @@
+package Beginner_Contest_209;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int a = Integer.parseInt(st.nextToken());
+		int b = Integer.parseInt(st.nextToken());
+		
+		if(a > b)
+			System.out.println(0);
+		else
+			System.out.println(b - a + 1);	
+	}
+}
