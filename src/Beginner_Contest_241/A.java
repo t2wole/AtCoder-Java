a7a125aa8850443c0d18d6821367b7b58b038197
t2wole <t2wole@gmail.com>
2022-05-08 09:05:13 +0900

    Digit Machine

---
+++ src/Beginner_Contest_241/A.java
@@ -0,0 +1,28 @@
+package Beginner_Contest_241;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class A {
+
+	public static void main(String[] args) throws IOException {
+		
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		int[] arr = new int[10];
+		
+		for(int i = 0; i < 10; i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		int first = arr[0];
+		int second = arr[first];
+		int third = arr[second];
+		
+		System.out.println(third);	
+	}
+}
