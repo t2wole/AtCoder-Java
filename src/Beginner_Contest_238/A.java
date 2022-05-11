6f61d1577e9ddb740b50401a9308286ce01978c5
t2wole <t2wole@gmail.com>
2022-05-11 10:26:50 +0900

    Exponential or Quadratic

---
+++ src/Beginner_Contest_238/A.java
@@ -0,0 +1,17 @@
+package Beginner_Contest_238;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class A {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		int n = Integer.parseInt(br.readLine());
+		
+		System.out.println(n >= 5 || n == 1 ? "Yes" : "No");
+	}
+}
